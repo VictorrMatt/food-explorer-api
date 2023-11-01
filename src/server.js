dcde871908f6e3e1407607c316f513a105b49a39
Victor Mateus <victormateus897@gmail.com>
2023-10-31 23:15:55 -0300

    Feat: Deleted remnants

--- src/server.js
+++ src/server.js
@@ -1,7 +1,6 @@
 require("express-async-errors");
 require("dotenv/config");
 
-const knex = require("./database/knex");
 const AppError = require("./utils/AppError");
 
 const uploadConfig = require("./configs/upload");
